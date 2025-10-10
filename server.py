5037dc1babdcfeb5ed26a449c368639f6982d0ae
Ilya <ilya.ilyukhin12@yandex.ru>
2025-10-10 18:28:52 +0300

    Add test window on PySide6

---
+++ server.py
@@ -0,0 +1,17 @@
+import sys
+from PySide6.QtWidgets import (QApplication, QSizePolicy, QMainWindow,)
+
+from ui_main import Ui_MainWindow
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
